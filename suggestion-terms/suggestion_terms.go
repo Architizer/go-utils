8911512aef20fb0e637ef13c862479dc18a195ad
Abhijit Pradhan <abhijit@architizer.com>
2018-04-30 11:50:13 -0400

    first commit

---
+++ suggestion-terms/suggestion_terms.go
@@ -0,0 +1,86 @@
+package suggestionterms
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/rtt/Go-Solr"
+)
+
+const defaultWeightField = "count_i"
+
+// SuggestionTerm represents a suggestion_terms document.
+type SuggestionTerm struct {
+	Field       string
+	Value       string
+	Count       int
+	WeightField string
+}
+
+// SuggestionTermCollection represents a collection of suggestion_terms documents
+type SuggestionTermCollection struct {
+	SuggestionTerms []SuggestionTerm
+	WeightField     string
+}
+
+// NewSuggestionTerm creates a SuggestionTerm from a facet name and FacentCount object
+func NewSuggestionTerm(facetName string, facetCount solr.FacetCount, weightField string) *SuggestionTerm {
+	st := new(SuggestionTerm)
+	st.Field = facetName
+	st.Value = strings.ToLower(facetCount.Value)
+	st.Count = facetCount.Count
+	st.WeightField = weightField
+	return st
+}
+
+// NewSuggestionTermCollection creates a SuggestionTermCollection and returns the ptr.
+func NewSuggestionTermCollection(weightField string) *SuggestionTermCollection {
+	if weightField == "" {
+		weightField = defaultWeightField
+	}
+	collection := new(SuggestionTermCollection)
+	collection.WeightField = weightField
+	return collection
+}
+
+// DocumentID returns a Solr document id for a SuggestionTerm
+func (st *SuggestionTerm) DocumentID() string {
+	return fmt.Sprint(st.Field, ".", st.Value)
+}
+
+// NewDocument converts a SuggestionTerm into a solr.Document
+func (st *SuggestionTerm) NewDocument() *solr.Document {
+	return &solr.Document{Fields: map[string]interface{}{
+		"id":           st.DocumentID(),
+		"term_s":       map[string]string{"set": st.Value},
+		st.WeightField: map[string]int{"set": st.Count},
+	}}
+}
+
+// AddSuggestionTerms adds SuggestionTerms to a SuggestionTermCollection
+// facet: solr.Facet to create suggestion terms from
+// weightField: Name of weight field on Solr document
+func (collection *SuggestionTermCollection) AddSuggestionTerms(facet solr.Facet, weightField string) error {
+	var facetCount solr.FacetCount
+	for i := 0; i < len(facet.Counts); i++ {
+		facetCount = facet.Counts[i]
+		st := NewSuggestionTerm(facet.Name, facetCount, weightField)
+		collection.SuggestionTerms = append(collection.SuggestionTerms, *st)
+	}
+	return nil
+}
+
+// ToUpdateDocument converts SuggestionTerms into a payload for solr.Connection.Update
+func (collection *SuggestionTermCollection) ToUpdateDocument() map[string]interface{} {
+	docs := make([]interface{}, len(collection.SuggestionTerms))
+
+	for i, st := range collection.SuggestionTerms {
+		docs[i] = st.NewDocument().Doc()
+	}
+
+	payload := map[string]interface{}{
+		"add": docs,
+	}
+
+	return payload
+}
---
+++ suggestion-terms/suggestion_terms_test.go
@@ -0,0 +1,260 @@
+package suggestionterms
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"testing"
+
+	"github.com/rtt/Go-Solr"
+)
+
+func TestNewSuggestionTerm(t *testing.T) {
+	type TestCase struct {
+		Got                 *SuggestionTerm
+		ExpectedValue       string
+		ExpectedWeightField string
+	}
+	testCases := []TestCase{
+		TestCase{
+			Got: NewSuggestionTerm(
+				"pseudotiers_ss",
+				solr.FacetCount{Value: "Furniture - Contract", Count: 300},
+				"brand_count_i",
+			),
+			ExpectedValue:       "furniture - contract",
+			ExpectedWeightField: "brand_count_i",
+		},
+		TestCase{
+			Got: NewSuggestionTerm(
+				"product_type_ent_ss",
+				solr.FacetCount{Value: "emergency lights", Count: 300},
+				"project_count_i",
+			),
+			ExpectedValue:       "emergency lights",
+			ExpectedWeightField: "project_count_i",
+		},
+	}
+	for _, tc := range testCases {
+		if tc.Got.Value != tc.ExpectedValue {
+			t.Error(
+				"FOR:\n", tc, "\n",
+				"EXPECTED:\n", tc.ExpectedValue, "\n",
+				"GOT:", tc.Got.Value, "\n",
+			)
+		}
+		if tc.Got.WeightField != tc.ExpectedWeightField {
+			t.Error(
+				"FOR:\n", tc, "\n",
+				"EXPECTED:\n", tc.ExpectedWeightField, "\n",
+				"GOT:", tc.Got.WeightField, "\n",
+			)
+		}
+	}
+
+}
+
+func TestDocumentID(t *testing.T) {
+	type TestCase struct {
+		Got      SuggestionTerm
+		Expected string
+	}
+
+	testCases := []TestCase{
+		TestCase{
+			Got: *NewSuggestionTerm(
+				"pseudotiers_ss",
+				solr.FacetCount{Value: "Furniture - Contract", Count: 300},
+				"count_i",
+			),
+			Expected: "pseudotiers_ss.furniture - contract",
+		},
+		TestCase{
+			Got: *NewSuggestionTerm(
+				"product_type_ent_ss",
+				solr.FacetCount{Value: "emergency lights", Count: 300},
+				"count_i",
+			),
+			Expected: "product_type_ent_ss.emergency lights",
+		},
+	}
+
+	for _, tc := range testCases {
+		got := tc.Got.DocumentID()
+
+		if got != tc.Expected {
+			t.Error(
+				"For", tc.Got,
+				"expected", tc.Expected,
+				"got", got,
+			)
+		}
+	}
+}
+
+func TestNewDocument(t *testing.T) {
+	type TestCase struct {
+		Got      SuggestionTerm
+		Expected *solr.Document
+	}
+
+	testCases := []TestCase{
+		TestCase{
+			Got: SuggestionTerm{"pseudotiers_ss", "lighting", 100, ""},
+			Expected: &solr.Document{
+				Fields: map[string]interface{}{
+					"id":      "pseudotiers_ss.lighting",
+					"term_s":  "lighting",
+					"count_i": 100,
+				},
+			},
+		},
+		TestCase{
+			Got: SuggestionTerm{"product_type_ent_ss", "wood doors", 100, ""},
+			Expected: &solr.Document{
+				Fields: map[string]interface{}{
+					"id":     "product_type_ent_ss.wood doors",
+					"term_s": "wood doors",
+				},
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		got := tc.Got.NewDocument()
+		if got.Fields["id"] != tc.Expected.Fields["id"] {
+			t.Error(
+				"For", tc.Got,
+				"expected", tc.Expected,
+				"got", got,
+			)
+		}
+	}
+}
+
+func TestAddSuggestionTerms(t *testing.T) {
+	type Arguments struct {
+		Facet       solr.Facet
+		WeightField string
+	}
+	type TestCase struct {
+		Collection *SuggestionTermCollection
+		Arguments  *Arguments
+		Expected   int
+	}
+
+	testCases := []TestCase{
+		TestCase{
+			Collection: NewSuggestionTermCollection("brand_count_i"),
+			Arguments: &Arguments{
+				Facet: solr.Facet{
+					Name: "pseudotiers_ss",
+					Counts: []solr.FacetCount{
+						solr.FacetCount{Value: "Lighting", Count: 100},
+						solr.FacetCount{Value: "Stone", Count: 100},
+					},
+				},
+				WeightField: "brand_count_i",
+			},
+			Expected: 2,
+		},
+		TestCase{
+			Collection: NewSuggestionTermCollection("project_count_i"),
+			Arguments: &Arguments{
+				Facet: solr.Facet{
+					Name: "product_type_ent_ss",
+					Counts: []solr.FacetCount{
+						solr.FacetCount{Value: "wood ceiling", Count: 100},
+						solr.FacetCount{Value: "folding doors", Count: 100},
+						solr.FacetCount{Value: "window trim", Count: 100},
+					},
+				},
+				WeightField: "project_count_i",
+			},
+			Expected: 3,
+		},
+	}
+
+	for _, tc := range testCases {
+		tc.Collection.AddSuggestionTerms(tc.Arguments.Facet, tc.Arguments.WeightField)
+		got := len(tc.Collection.SuggestionTerms)
+		if got != tc.Expected {
+			t.Error(
+				"For", tc.Arguments,
+				"expected", tc.Expected,
+				"got", got,
+			)
+		}
+	}
+}
+
+func TestToUpdateDocuments(t *testing.T) {
+	type TestCase struct {
+		Collection    *SuggestionTermCollection
+		ExpectedBytes []byte
+	}
+
+	expectedBytes, err := json.Marshal(map[string]interface{}{
+		"add": []interface{}{
+			map[string]interface{}{
+				"id":            "pseudotiers_ss.lighting",
+				"term_s":        map[string]string{"set": "lighting"},
+				"brand_count_i": map[string]int{"set": 100},
+			},
+			map[string]interface{}{
+				"id":            "pseudotiers_ss.plumbing",
+				"term_s":        map[string]string{"set": "plumbing"},
+				"brand_count_i": map[string]int{"set": 100},
+			},
+		},
+	})
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	stCollection := NewSuggestionTermCollection("brand_count_i")
+	_ = stCollection.AddSuggestionTerms(
+		solr.Facet{
+			Name: "pseudotiers_ss",
+			Counts: []solr.FacetCount{
+				solr.FacetCount{
+					Value: "lighting",
+					Count: 100,
+				},
+				solr.FacetCount{
+					Value: "plumbing",
+					Count: 100,
+				},
+			},
+		},
+		"brand_count_i",
+	)
+
+	testCases := []TestCase{
+		TestCase{
+			Collection:    stCollection,
+			ExpectedBytes: expectedBytes,
+		},
+	}
+
+	for _, tc := range testCases {
+		payload := tc.Collection.ToUpdateDocument()
+		gotBytes, err := solr.JSONToBytes(payload)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		got := string(*gotBytes)
+		expected := string(tc.ExpectedBytes)
+		if bytes.Compare(tc.ExpectedBytes, *gotBytes) != 0 {
+			t.Error(
+				"EXPECTED:\n", expected,
+				"\n",
+				"GOT:\n", got,
+				"\n",
+			)
+		}
+	}
+}
